e35c5dc2fcd5b6553eb16683b421e70e5c6c7d5f
DanielG-H <danielgarcia.alumno@gmail.com>
2020-11-04 20:44:00 -0800

    Added 'books read' and 'books to read' buttons and frame

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from tkinter import *
+from tkinter import ttk
 
 
-def main():
-    root = Tk()
-    root.mainloop()
+class App:
+    def __init__(self, root):
+        self.win = root
+        self.win.title("Book Register App")
+
+        # Create options for "books read" and "books to read"
+        frame_main = LabelFrame(self.win, text="Select an option")
+        frame_main.grid(row=0, column=0, columnspan=2, padx=70, pady=40)
+
+        # Select options
+        books_read = Button(frame_main, text="Books Read", fg="gray10", bg="gold", padx=65, pady=30)
+        books_read.grid(row=0, column=0, padx=5, pady=5)
+
+        books_to_read = Button(frame_main, text="Books To Read", fg="gray10", bg="goldenrod", padx=65, pady=30)
+        books_to_read.grid(row=0, column=1, padx=5, pady=5)
 
 
 if __name__ == "__main__":
-    main()
+    root = Tk()
+    app = App(root)
+    root.mainloop()
