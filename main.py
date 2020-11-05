edd70f6a6490108303fff57c35818e4e6f07f3a8
DanielG-H <danielgarcia.alumno@gmail.com>
2020-11-04 23:02:09 -0800

    Added the two windows for the options 'read' and 'to read' books and buttons to return to main menu and prevent the mainloop from closing without said button

--- main.py
+++ main.py
@@ -6,16 +6,43 @@ class App:
     def __init__(self, window):
         self.win = window
         self.win.title("Book Register App")
+        self.display_options()
 
+    def disable_event(self):
+        pass
+
+    def exit_open_menu(self, opt):
+        self.win.protocol("WM_DELETE_WINDOW", self.disable_event)
+        close_menu = Button(opt, text="Close Main Menu", command=self.win.withdraw)
+        close_menu.grid(row=1, column=1, padx=5, pady=5)
+
+        go_to_menu = Button(opt, text="Go To Menu", command=lambda: [self.win.deiconify(), opt.withdraw()])
+        go_to_menu.grid(row=1, column=2, padx=5, pady=5)
+
+    def open_read_window(self):
+        read = Toplevel()
+        read.title("Book Register App - [Books Read]")
+        self.exit_open_menu(read)
+        read.mainloop()
+
+    def open_to_read_window(self):
+        to_read = Toplevel()
+        to_read.title("Book Register App - [Books To Read]")
+        self.exit_open_menu(to_read)
+        to_read.mainloop()
+
+    def display_options(self):
         # Create options for "books read" and "books to read"
-        frame_main = LabelFrame(self.win, text="Select an option")
+        frame_main = LabelFrame(self.win, text="Select an option: ")
         frame_main.grid(row=0, column=0, columnspan=2, padx=70, pady=40)
 
         # Select options
-        books_read = Button(frame_main, text="Books Read", fg="gray10", bg="gold", padx=65, pady=30)
+        books_read = Button(frame_main, text="Books Read", fg="gray10", bg="gold",
+                            padx=65, pady=30, command=self.open_read_window)
         books_read.grid(row=0, column=0, padx=5, pady=5)
 
-        books_to_read = Button(frame_main, text="Books To Read", fg="gray10", bg="goldenrod", padx=65, pady=30)
+        books_to_read = Button(frame_main, text="Books To Read", fg="gray10", bg="goldenrod",
+                               padx=65, pady=30, command=self.open_to_read_window)
         books_to_read.grid(row=0, column=1, padx=5, pady=5)
 
 
