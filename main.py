d474ac1bb9ab38b0e25b0bbfde0b22485f3f1655
DanielG-H <danielgarcia.alumno@gmail.com>
2020-11-07 00:15:12 -0800

    Added a table to be displayed in the book record window, set the options' background colors and adjusted the table layout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter import ttk
 
+# import sqlite3
+
 
 class App:
     def __init__(self, window):
@@ -38,17 +40,22 @@ class App:
     def exit_open_menu(self, opt, button):
         # prevents exiting the main menu without a button
         opt.protocol("WM_DELETE_WINDOW", self.disable_event)
-        close_menu = Button(opt, text="Close Main Menu", command=self.win.withdraw)
-        close_menu.grid(row=0, column=0)
+
+        exit_frame = LabelFrame(opt, text="Menu Options: ")
+        exit_frame.grid(column=2, row=0)
+
+        close_menu = Button(exit_frame, text="Close Main Menu", command=self.win.withdraw)
+        close_menu.grid(row=0, column=0, pady=10)
 
         # goes to main menu and closes the other window
-        go_to_menu = Button(opt, text="Go To Main Menu",
+        go_to_menu = Button(exit_frame, text="Go To Main Menu",
                             command=lambda: [self.win.deiconify(), opt.withdraw(), self.enable_button(button)])
-        go_to_menu.grid(row=0, column=1)
+        go_to_menu.grid(row=0, column=1, pady=10)
 
     def open_read_window(self, button):
         read = Toplevel()
         read.title("Book Register App - [Books Read]")
+        read.config(background="gold")
         self.disable_button(button)
         self.exit_open_menu(read, button)
 
@@ -60,6 +67,7 @@ class App:
     def open_to_read_window(self, button):
         to_read = Toplevel()
         to_read.title("Book Register App - [Books To Read]")
+        to_read.config(background="goldenrod")
         self.disable_button(button)
         self.exit_open_menu(to_read, button)
 
@@ -79,7 +87,7 @@ class App:
     # Decides the table to be modified using a data key (indicates option)
     def data_manipulation(self, opt, data_key):
         data_frame = Frame(opt)
-        data_frame.grid(row=1, column=0, columnspan=2)
+        data_frame.grid(row=1, column=2, columnspan=2)
 
         Label(data_frame, text="Book: ").grid(row=1, column=0)
         Entry(data_frame).grid(row=1, column=1)
@@ -90,13 +98,21 @@ class App:
         Label(data_frame, text="Genre: ").grid(row=3, column=0)
         Entry(data_frame).grid(row=3, column=1)
 
-        if data_key != 0:
+        if data_key != 1:
             Label(data_frame, text="Rating: ").grid(row=4, column=0)
-            Entry(data_frame).grid(row=4, column=1)
+            Entry(data_frame).grid(row=4, column=1, columnspan=2)
 
         # command=lambda: self.register(opt, data_key)
-        register = Button(opt, text="Register")
-        register.grid(row=4, column=0, columnspan=2)
+        register = Button(opt, text="Register", padx=30, pady=10)
+        register.grid(row=5, column=1, columnspan=2, pady=10)
+
+        main_table = ttk.Treeview(opt, height=10, columns=(0, 1, 2))
+        main_table.grid(row=6, column=2, sticky="nsew", padx=10, pady=10)
+
+        main_table.heading("#0", text="Book", anchor=CENTER)
+        main_table.heading("#1", text="Author", anchor=CENTER)
+        main_table.heading("#2", text="Genre", anchor=CENTER)
+        main_table.heading("#3", text="Rating", anchor=CENTER)
 
     # def register(self, opt, data_key):
     #     if data_key == 0:
